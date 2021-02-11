7bf8596475509d217147a9321c9d8c5559fb844b
TateFowble <tatefowble@gmail.com>
2021-02-11 05:43:12 -0600

    testing the waters

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+  // CSS imports 
 import './App.css';
-
+  // Library imports
 import {
   HashRouter as Router,
   Switch,
   Route
 } from 'react-router-dom';
 
-
+  // Page imports
 import HomePage from './pages/HomePage';
 import ErrorPage from './pages/ErrorPage';
 
