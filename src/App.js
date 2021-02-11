924ece575f7d8afc66989770db3430f81cf5955e
TateFowble <tatefowble@gmail.com>
2021-02-11 04:52:05 -0600

    testing the waters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+
+import Homepage from './pages/Homepage';
+
 function App() {
   return (
    <div>
-     Hello!
+     <Router>
+      <Switch>
+        <Route path='/'>
+          <Homepage />
+          </Route>
+      </Switch>
+      </Router>
    </div>
   );
 }
---
+++ src/pages/Homepage.js
@@ -0,0 +1,15 @@
+import { React } from 'react';
+
+
+
+
+
+const Homepage = () => {
+    return (
+        <div>
+            <h1>Hello!</h1>
+        </div>
+    )
+}
+
+export default Homepage;
