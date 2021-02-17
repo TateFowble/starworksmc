2a593fdf3e08d517fb214a25e45e96880b3a88a6
TateFowble <tatefowble@gmail.com>
2021-02-17 03:42:57 -0600

    FINIALLY ADDED SOME DESIGN

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -6,8 +6,16 @@ import Navigation from '../components/navigation/navigation';
 
 const HomePage = () => {
     return (
-        <div className='w-100 h-100 homepage-background'>
-            <div className='w-50' style={{backgroundColor:'blue'}}></div>
+        <div>
+            <div className='w-100 h-100 homepage-background'>
+                <Navigation />
+                <div className='w-100 h-10'></div>
+                <div className='w-20 h-10 fl-l inline '></div>
+                <div className='w-60 h-10 fl-l inline text-center'>
+                    <h1 style={{fontSize:'72px',color:'white'}}>PLAY.STARWORKSMC.US</h1>
+                </div>
+                <div className='w-20 h-10 fl-l inline'></div>
+            </div>
         </div>
     )
 }
