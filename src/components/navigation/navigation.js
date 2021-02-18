c9cb50032e69723787ade2450af574bf36c50528
TateFowble <tatefowble@gmail.com>
2021-02-18 17:29:02 -0600

    updated changes

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -6,8 +6,9 @@ import './navigation.css';
 const navigation = () => {
     return (
         <div>
-            <div className='w-100 navigation-height'>
-                <div className="w-30">
+            <div className='w-100 h-10'>
+                <div className="w-30 fl-r check">
+                    Login
                 </div>
             </div>
         </div>
